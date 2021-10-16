59c55b0699b01f67c005b858689dca69ae8c216f
Thanh Le <lenguyenthanh@hotmail.com>
2021-10-16 12:48:27 +0200

    Add cats-parse dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,11 @@ object Dependencies {
   object Cats {
     val catsEffect = "org.typelevel" %% "cats-effect" % "3.2.9"
     val catsMtl = "org.typelevel" %% "cats-mtl" % "1.2.1"
+    val catsParse = "org.typelevel" %% "cats-parse" % "0.3.4"
+
     val munitCatsEffect = "org.typelevel" %% "munit-cats-effect-3" % "1.0.6" % Test
 
-    val all = Seq(catsEffect, catsMtl, munitCatsEffect)
+    val all = Seq(catsEffect, catsMtl, catsParse, munitCatsEffect)
   }
 
 }
