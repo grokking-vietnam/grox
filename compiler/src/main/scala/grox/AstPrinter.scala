5b37992e6b72c94d7c535e37c406291c6b68faca
akizminet <akizminet@gmail.com>
2022-01-06 21:56:22 +0700

    chore: fix style

--- compiler/src/main/scala/grox/AstPrinter.scala
+++ compiler/src/main/scala/grox/AstPrinter.scala
@@ -1,7 +1,9 @@
 package grox
 
-import scala.annotation.tailrec
 import javax.naming.BinaryRefAddr
+
+import scala.annotation.tailrec
+
 import Expr.*
 
 object AstPrinter:
@@ -9,13 +11,13 @@ object AstPrinter:
   final def print(expr: Expr): String =
     expr match
       case Number(value) => value.toString
-      case Str(value) => value.toString
-      case Negate(e) => s"(! ${print(e)})"
-      case Minus(e) => s"(- ${print(e)})"
-      case Grouping(e) => s"(group ${print(e)})"
+      case Str(value)    => value.toString
+      case Negate(e)     => s"(! ${print(e)})"
+      case Minus(e)      => s"(- ${print(e)})"
+      case Grouping(e)   => s"(group ${print(e)})"
 
       // Binary
-      case Add(left,right) => s"(+ ${print(left)} ${print(right)})"
-      case Subtract(left,right) => s"(- ${print(left)} ${print(right)})"
-      case Multiply(left,right) => s"(* ${print(left)} ${print(right)})"
-      case Divide(left,right) => s"(/ ${print(left)} ${print(right)})"
+      case Add(left, right)      => s"(+ ${print(left)} ${print(right)})"
+      case Subtract(left, right) => s"(- ${print(left)} ${print(right)})"
+      case Multiply(left, right) => s"(* ${print(left)} ${print(right)})"
+      case Divide(left, right)   => s"(/ ${print(left)} ${print(right)})"
--- compiler/src/test/scala/grox/AstPrinterTest.scala
+++ compiler/src/test/scala/grox/AstPrinterTest.scala
@@ -1,7 +1,8 @@
 package grox
 
-import Expr._
 import munit.CatsEffectSuite
+
+import Expr._
 
 class AstPrinterTest extends munit.FunSuite:
   test("Example 1 + 2") {
@@ -16,10 +17,7 @@ class AstPrinterTest extends munit.FunSuite:
   test("Example -123*(45.67)") {
     assertEquals(
       AstPrinter.print(
-        Multiply(
-          Minus(
-            Number(123)),
-          Grouping(Number(45.67)))
+        Multiply(Minus(Number(123)), Grouping(Number(45.67)))
       ),
       "(* (- 123) (group 45.67))",
     )
