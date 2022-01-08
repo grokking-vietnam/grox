ec5ebab415d45558bc64a777d65742e7b72879b2
Thanh Le <lenguyenthanh@hotmail.com>
2022-01-08 10:27:03 +0100

    Delete printer

--- compiler/src/main/scala/grox/Expr.scala
+++ compiler/src/main/scala/grox/Expr.scala
@@ -19,33 +19,34 @@ enum Expr[T]:
   // string ++ Boolean => error
   case Concat(left: Expr[String], right: Expr[String]) extends Expr[String]
 
-  // val a = 3.0 => DoubleVariable()
-  // val a = 3.0 <= 4 => BoolVariable
-  // val a = 3.0 <= True => Error
-  // a = True => error
-  //case DoubleVariable(value: String) extends Expr[Double]
+// val a = 3.0 => DoubleVariable()
+// val a = 3.0 <= 4 => BoolVariable
+// val a = 3.0 <= True => Error
+// a = True => error
+//case DoubleVariable(value: String) extends Expr[Double]
 
-  //case BoolVariable(value: String) extends Expr[Boolean]
+//case BoolVariable(value: String) extends Expr[Boolean]
 
-  //case Subtract(left: Expr, right: Expr)
-  //case Multiply(left: Expr, right: Expr)
-  //case Divide(left: Expr, right: Expr)
-  //case Negate(expr: Expr)
-  //case Minus(expr: Expr)
-  //case Boolean(value: Boolean)
-  //case Str(value: String)
-  //case Grouping(expr: Expr)
+//case Subtract(left: Expr, right: Expr)
+//case Multiply(left: Expr, right: Expr)
+//case Divide(left: Expr, right: Expr)
+//case Negate(expr: Expr)
+//case Minus(expr: Expr)
+//case Boolean(value: Boolean)
+//case Str(value: String)
+//case Grouping(expr: Expr)
 
 object Expr {
 
-  def eval[T](expr: Expr[T]): T = expr match {
-    case Add(left, right) => eval(left) + eval(right)
-    case Greater(left, right) => eval(left) > eval(right)
-    case Bool(b) => b
-    case Negate(e) => - eval(expr)
-    case Number(d) => d
-    case Concat(l, r) => eval(l) ++ eval(r)
-  }
+  def eval[T](expr: Expr[T]): T =
+    expr match {
+      case Add(left, right)     => eval(left) + eval(right)
+      case Greater(left, right) => eval(left) > eval(right)
+      case Bool(b)              => b
+      case Negate(e)            => -eval(expr)
+      case Number(d)            => d
+      case Concat(l, r)         => eval(l) ++ eval(r)
+    }
 
   def example() = {
     val expr = Add(Number(3), Number(4))
