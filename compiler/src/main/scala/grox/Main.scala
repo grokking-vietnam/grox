c884884a60b0e208e541c08211eeed9936eff68e
anhlt <tuananh.kirimaru@gmail.com>
2022-01-17 00:45:26 +0900

    Update Main class

--- compiler/src/main/scala/grox/Main.scala
+++ compiler/src/main/scala/grox/Main.scala
@@ -1,7 +1,34 @@
 package grox
 
-import cats.effect.{IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
 
-object Main extends IOApp.Simple {
-  def run: IO[Unit] = IO.println("Hello grox")
+import com.monovore.decline._
+import com.monovore.decline.effect._
+
+object Main
+  extends CommandIOApp(
+    name = "grox",
+    header = "grox compiler",
+    version = "0.0.1",
+  ) {
+
+  override def main: Opts[IO[ExitCode]] = {
+    val helpOpts = Opts.flag("help", help = "Help String").orFalse
+
+    helpOpts.map { helpOption =>
+      for {
+        _ <-
+          if (helpOption) {
+            IO.println("Help")
+          } else {
+            IO.println("Hello grox")
+          }
+        exitCode <- IO(ExitCode.Success)
+      } yield exitCode
+
+    }
+
+  }
+
 }
