c0f51b2389cd510093f1ce21b664dfb2676ef90f
anhlt <tuananh.kirimaru@gmail.com>
2022-01-16 23:01:33 +0900

    update dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,20 @@ import sbt._
 
 object Dependencies {
 
-  object Cats {
-    val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.5"
-    val catsMtl = "org.typelevel" %% "cats-mtl" % "1.2.1"
-    val catsParse = "org.typelevel" %% "cats-parse" % "0.3.6"
+  val declineVersion = "2.2.0"
+  val catsEffectVersion =  "3.3.5"
+  val catsMtlVersion = "1.2.1"
+  val catsParseVersion = "0.3.6"
 
-    val all = Seq(catsEffect, catsMtl, catsParse)
+
+  object Cats {
+    val catsEffect = "org.typelevel" %% "cats-effect" % catsEffectVersion
+    val catsMtl = "org.typelevel" %% "cats-mtl" % catsMtlVersion
+    val catsParse = "org.typelevel" %% "cats-parse" % catsParseVersion
+    val decline = "com.monovore" %% "decline" % declineVersion
+    val declineEffect = "com.monovore" %% "decline-effect" % declineVersion
+
+    val all = Seq(catsEffect, catsMtl, catsParse, decline, declineEffect)
   }
 
   object Tests {
