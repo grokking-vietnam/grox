dd307a23484d98e98f0b9af699fe9e751e0c7046
Thanh Le <lenguyenthanh@hotmail.com>
2022-02-24 08:21:06 +0100

    Add And, Or operator

--- compiler/src/main/scala/grox/Expr.scala
+++ compiler/src/main/scala/grox/Expr.scala
@@ -9,6 +9,8 @@ enum Expr:
   case Subtract(left: Expr, right: Expr)
   case Multiply(left: Expr, right: Expr)
   case Divide(left: Expr, right: Expr)
+  case And(left: Expr, right: Expr)
+  case Or(left: Expr, right: Expr)
 
   // Unary
   case Negate(expr: Expr)
