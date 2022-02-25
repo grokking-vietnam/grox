93760df482b70595dd3247d86cd61483786fe779
Thanh Le <lenguyenthanh@hotmail.com>
2022-02-25 19:54:46 +0100

    Add comparison operators

--- compiler/src/main/scala/grox/Expr.scala
+++ compiler/src/main/scala/grox/Expr.scala
@@ -5,10 +5,20 @@ type LiteralType = Double | String | Boolean | Null
 enum Expr:
 
   // Binary
+
+  // arithmetic
   case Add(left: Expr, right: Expr)
   case Subtract(left: Expr, right: Expr)
   case Multiply(left: Expr, right: Expr)
   case Divide(left: Expr, right: Expr)
+
+  // comparison
+  case Greater(left: Expr, right: Expr)
+  case GreaterEqual(left: Expr, right: Expr)
+  case Less(left: Expr, right: Expr)
+  case LessEqual(left: Expr, right: Expr)
+  case Equal(left: Expr, right: Expr)
+  case NotEqual(left: Expr, right: Expr)
 
   // Unary
   case Negate(expr: Expr)
