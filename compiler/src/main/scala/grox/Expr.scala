3fa012adcb562024a7b56fa18e8149092f5a32bf
Thanh Le <lenguyenthanh@hotmail.com>
2022-02-26 11:08:48 +0100

    Reimplement Expr follow the book (#84)
    * Reimplement Expr follow the book
    
    * Update grammar

--- compiler/src/main/scala/grox/Expr.scala
+++ compiler/src/main/scala/grox/Expr.scala
@@ -1,62 +1,31 @@
 package grox
 
-// Todo add support for:
-// [ ] String literal
-// [ ] String operators
-// [ ] comparision operators with Order
-// [ ] variables
+type LiteralType = Double | String | Boolean | Null
 
-enum Expr[T]:
+enum Expr:
 
-  // math operators
-  case Plus(left: Expr[Double], right: Expr[Double]) extends Expr[Double]
-  case Minus(left: Expr[Double], right: Expr[Double]) extends Expr[Double]
-  case Times(left: Expr[Double], right: Expr[Double]) extends Expr[Double]
-  case Divide(left: Expr[Double], right: Expr[Double]) extends Expr[Double]
-  case Negate(expr: Expr[Double]) extends Expr[Double]
+  // Binary
 
-  // logic operators
-  case Not(expr: Expr[Boolean]) extends Expr[Boolean]
-  case And(left: Expr[Boolean], right: Expr[Boolean]) extends Expr[Boolean]
-  case Or(left: Expr[Boolean], right: Expr[Boolean]) extends Expr[Boolean]
+  // arithmetic
+  case Add(left: Expr, right: Expr)
+  case Subtract(left: Expr, right: Expr)
+  case Multiply(left: Expr, right: Expr)
+  case Divide(left: Expr, right: Expr)
 
-  // comparision operators
-  case Greater(left: Expr[Double], right: Expr[Double]) extends Expr[Boolean]
-  case GreaterEqual(left: Expr[Double], right: Expr[Double]) extends Expr[Boolean]
-  case Less(left: Expr[Double], right: Expr[Double]) extends Expr[Boolean]
-  case LessEqual(left: Expr[Double], right: Expr[Double]) extends Expr[Boolean]
-  case Equal(left: Expr[T], right: Expr[T]) extends Expr[Boolean]
-  case NotEqual(left: Expr[T], right: Expr[T]) extends Expr[Boolean]
+  // comparison
+  case Greater(left: Expr, right: Expr)
+  case GreaterEqual(left: Expr, right: Expr)
+  case Less(left: Expr, right: Expr)
+  case LessEqual(left: Expr, right: Expr)
+  case Equal(left: Expr, right: Expr)
+  case NotEqual(left: Expr, right: Expr)
 
-  case Grouping(expr: Expr[T]) extends Expr[T]
+  // Unary
+  case Negate(expr: Expr)
+  case Not(expr: Expr)
 
-  case Bool(value: Boolean) extends Expr[Boolean]
-  case Num(value: Double) extends Expr[Double]
+  case Literal(value: LiteralType)
 
-  // case BoolVal(name: String) extends Expr[Boolean]
-  // case NumVal(name: String) extends Expr[Double]
+  case Grouping(expr: Expr)
 
-object Expr {
-
-  def eval[T](expr: Expr[T]): T =
-    expr match {
-      case Plus(left, right)         => eval(left) + eval(right)
-      case Minus(left, right)        => eval(left) - eval(right)
-      case Times(left, right)        => eval(left) * eval(right)
-      case Divide(left, right)       => eval(left) / eval(right)
-      case Negate(expr)              => -eval(expr)
-      case Not(expr)                 => !eval(expr)
-      case And(left, right)          => eval(left) && eval(right)
-      case Or(left, right)           => eval(left) || eval(right)
-      case Greater(left, right)      => eval(left) > eval(right)
-      case GreaterEqual(left, right) => eval(left) >= eval(right)
-      case Less(left, right)         => eval(left) < eval(right)
-      case LessEqual(left, right)    => eval(left) <= eval(right)
-      case Equal(left, right)        => eval(left) == eval(right)
-      case NotEqual(left, right)     => eval(left) != eval(right)
-      case Grouping(expr)            => eval(expr)
-      case Bool(value)               => value
-      case Num(value)                => value
-    }
-
-}
+object Expr {}
