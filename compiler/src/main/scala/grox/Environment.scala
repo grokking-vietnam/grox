e6c122c9f9e75d8c321e6af0658691ffa8f514e4
thanguyen <thanguyen@axon.com>
2022-04-16 16:56:57 +0700

    reformat code

--- compiler/src/main/scala/grox/Environment.scala
+++ compiler/src/main/scala/grox/Environment.scala
@@ -7,29 +7,36 @@ object Environment {
 }
 
 enum EnvironmentError(msg: String):
-  case UndefinedVariableError(variable: Token) extends EnvironmentError(s"Undefined variable: '$variable'.")
+  case UndefinedVariableError(variable: Token)
+    extends EnvironmentError(s"Undefined variable: '$variable'.")
 
-class Environment(private val values: Map[String, Token], private val enclosing: Option[Environment]) {
+class Environment(
+  private val values: Map[String, Token],
+  private val enclosing: Option[Environment],
+) {
 
-  def define(name: String, value: Token): Environment = {
+  def define(name: String, value: Token): Environment =
     new Environment(values + (name -> value), enclosing)
-  }
 
-  def get(name: Token): Option[Token] = {
+  def get(name: Token): Option[Token] =
     values.get(name.lexeme).orElse(enclosing.flatMap(_.get(name)))
-  }
 
   def assign(name: Token, value: Token): Either[EnvironmentError, Environment] = {
-    val assignEither = if (values.contains(name.lexeme)) {
-      Right(new Environment(values + (name.lexeme -> value), enclosing))
-    } else {
-      Left(EnvironmentError.UndefinedVariableError(name))
-    }
+    val assignEither =
+      if (values.contains(name.lexeme)) {
+        Right(new Environment(values + (name.lexeme -> value), enclosing))
+      } else {
+        Left(EnvironmentError.UndefinedVariableError(name))
+      }
 
-    assignEither.recoverWith {
-      case _ => enclosing.map(_.assign(name, value)
-                         .map(newEnclosing => new Environment(values, Some(newEnclosing))))
-                         .getOrElse(Left(EnvironmentError.UndefinedVariableError(name)))
+    assignEither.recoverWith { case _ =>
+      enclosing
+        .map(
+          _.assign(name, value)
+            .map(newEnclosing => new Environment(values, Some(newEnclosing)))
+        )
+        .getOrElse(Left(EnvironmentError.UndefinedVariableError(name)))
     }
   }
+
 }
