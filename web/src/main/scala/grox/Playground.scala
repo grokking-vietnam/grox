bbca5f5b3e15146968199c06ffaf49800678544b
Thanh Le <lenguyenthanh@hotmail.com>
2022-06-11 13:33:38 +0200

    Remove unnecesarry functions

--- web/src/main/scala/grox/Playground.scala
+++ web/src/main/scala/grox/Playground.scala
@@ -14,22 +14,17 @@ object Playground extends TyrianApp[Msg, Model]:
 
   val exec = Executor.module[Either[Throwable, *]]
 
-  def scan[F[_]: Functor](
-    exec: Executor[F],
-    str: String,
-  ): F[String] = exec.scan(str).map(tokens => tokens.mkString("\n"))
-
-  def parse[F[_]: Functor](exec: Executor[F], str: String): F[String] = exec.parse(str).map(_.show)
-
   def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) = (Model("", ""), Cmd.None)
 
   def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
     case Msg.Update(str) => (model.copy(input = str, result = ""), Cmd.None)
     case Msg.Scan =>
-      val result = scan(exec, model.input).fold(t => s"Error: ${t.toString}", identity)
+      val result = exec
+        .scan(model.input)
+        .fold(t => s"Error: ${t.toString}", tokens => tokens.mkString("\n"))
       (model.copy(result = result), Cmd.None)
     case Msg.Parse =>
-      val result = parse(exec, model.input).fold(t => s"Error: ${t.toString}", identity)
+      val result = exec.parse(model.input).fold(t => s"Error: ${t.toString}", _.show)
       (model.copy(result = result), Cmd.None)
 
   def view(model: Model): Html[Msg] = div(
