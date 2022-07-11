881c8c310077c1cbedc8dc7e8fdf4115a57d60c0
Thanh Le <lenguyenthanh@hotmail.com>
2022-07-12 01:33:07 +0200

    Add Span to Expr

--- compiler/shared/src/test/scala/grox/ExprFlatten.scala
+++ compiler/shared/src/test/scala/grox/ExprFlatten.scala
@@ -1,33 +1,34 @@
 package grox
 
 import Token.*
+import Span.*
 
 extension (e: Expr)
 
-  def binary(operator: Token[Unit], l: Expr, r: Expr) = l.flatten ::: List(operator) ::: r.flatten
+  def binary(operator: Token[Span], l: Expr, r: Expr) = l.flatten ::: List(operator) ::: r.flatten
 
-  def flatten: List[Token[Unit]] =
+  def flatten: List[Token[Span]] =
     e match
-      case Expr.Add(l, r)      => binary(Plus(()), l, r)
-      case Expr.Subtract(l, r) => binary(Minus(()), l, r)
-      case Expr.Multiply(l, r) => binary(Star(()), l, r)
-      case Expr.Divide(l, r)   => binary(Slash(()), l, r)
+      case Expr.Add(tag, l, r)      => binary(Plus(tag), l, r)
+      case Expr.Subtract(tag, l, r) => binary(Minus(tag), l, r)
+      case Expr.Multiply(tag, l, r) => binary(Star(tag), l, r)
+      case Expr.Divide(tag, l, r)   => binary(Slash(tag), l, r)
 
-      case Expr.Equal(l, r)    => binary(EqualEqual(()), l, r)
-      case Expr.NotEqual(l, r) => binary(BangEqual(()), l, r)
+      case Expr.Equal(tag, l, r)    => binary(EqualEqual(tag), l, r)
+      case Expr.NotEqual(tag, l, r) => binary(BangEqual(tag), l, r)
 
-      case Expr.Greater(l, r)      => binary(Greater(()), l, r)
-      case Expr.GreaterEqual(l, r) => binary(GreaterEqual(()), l, r)
-      case Expr.Less(l, r)         => binary(Less(()), l, r)
-      case Expr.LessEqual(l, r)    => binary(LessEqual(()), l, r)
+      case Expr.Greater(tag, l, r)      => binary(Greater(tag), l, r)
+      case Expr.GreaterEqual(tag, l, r) => binary(GreaterEqual(tag), l, r)
+      case Expr.Less(tag, l, r)         => binary(Less(tag), l, r)
+      case Expr.LessEqual(tag, l, r)    => binary(LessEqual(tag), l, r)
 
-      case Expr.Negate(e) => Minus(()) :: e.flatten
-      case Expr.Not(e)    => Bang(()) :: e.flatten
+      case Expr.Negate(tag, e) => Minus(tag) :: e.flatten
+      case Expr.Not(tag, e)    => Bang(tag) :: e.flatten
 
-      case Expr.Grouping(e) => List(LeftParen(())) ::: e.flatten ::: List(RightParen(()))
+      case Expr.Grouping(e) => List(LeftParen(empty)) ::: e.flatten ::: List(RightParen(empty))
 
-      case Expr.Literal(n: Double) => List(Number(n.toString, (())))
-      case Expr.Literal(s: String) => List(Number(s, (())))
-      case Expr.Literal(true)      => List(True(()))
-      case Expr.Literal(false)     => List(False(()))
-      case Expr.Literal(())        => List(Null(()))
+      case Expr.Literal(tag, n: Double) => List(Number(n.toString, tag))
+      case Expr.Literal(tag, s: String) => List(Number(s, tag))
+      case Expr.Literal(tag, true)      => List(True(tag))
+      case Expr.Literal(tag, false)     => List(False(tag))
+      case Expr.Literal(tag, ())        => List(Null(tag))
