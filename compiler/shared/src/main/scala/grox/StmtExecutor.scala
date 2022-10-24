3ec1321fc6e48a3a7303d18bc7e708f0335c3467
Thanh Le <lenguyenthanh@hotmail.com>
2022-10-24 17:01:08 +0200

    StmtOutput

--- compiler/shared/src/main/scala/grox/StmtExecutor.scala
+++ compiler/shared/src/main/scala/grox/StmtExecutor.scala
@@ -4,10 +4,20 @@ import cats.effect.kernel.Ref
 import cats.effect.std.Console
 import cats.syntax.all.*
 import cats.{Monad, MonadThrow}
+import fs2.Stream
+
+type Output = LiteralType
+type StmtOutput[F[_]] = F[(LiteralType, Stream[F, Output])]
+
+object StmtOutput:
+  def empty[F[_]]() = ((), Stream.empty[F])
 
 trait StmtExecutor[F[_]]:
   def execute(stmts: List[Stmt]): F[Unit]
   def execute(stmt: Stmt): F[LiteralType]
+
+  def execute1(stmt: Stmt): StmtOutput[F]
+  def execute1(stmts: List[Stmt]): F[Stream[F, Output]]
 
 object StmtExecutor:
   import Stmt.*
@@ -21,6 +31,7 @@ object StmtExecutor:
 
       def execute(stmt: Stmt): F[LiteralType] =
         stmt match
+
           case Block(stmts) =>
             for
               _ <- env.startBlock()
@@ -39,7 +50,7 @@ object StmtExecutor:
               state <- env.state
               result <- interpreter.evaluate(state, expr)
               _ <- Console[F].println(result)
-            yield result
+            yield ()
 
           case Var(name, init) =>
             for
@@ -76,3 +87,59 @@ object StmtExecutor:
           case Function(name, params, body) => ???
 
       def execute(stmts: List[Stmt]): F[Unit] = stmts.traverse_(execute)
+
+      def execute1(stmt: Stmt): StmtOutput[F] =
+        stmt match
+
+          case Block(stmts) =>
+            for
+              _ <- env.startBlock()
+              result <- execute1(stmts)
+              _ <- env.endBlock()
+            yield ((), result)
+
+          case Expression(expr) =>
+            for
+              state <- env.state
+              result <- interpreter.evaluate(state, expr)
+            yield (result, Stream.empty)
+
+          case Print(expr) =>
+            for
+              state <- env.state
+              result <- interpreter.evaluate(state, expr)
+            yield (result, Stream.eval(Monad[F].pure(result)))
+
+          case Var(name, init) =>
+            for
+              state <- env.state
+              result <- init.map(interpreter.evaluate(state, _)).sequence
+              _ <- env.define(name.lexeme, result.getOrElse(()))
+            yield ((), Stream.empty)
+
+          case Assign(name, value) =>
+            for
+              state <- env.state
+              result <- interpreter.evaluate(state, value)
+              _ <- env.assign(name, result)
+            yield StmtOutput.empty[F]()
+
+          case While(cond, body) =>
+            val conditionStmt =
+              for
+                state <- env.state
+                r <- interpreter.evaluate(state, cond)
+              yield r.isTruthy
+            val bodyStmt: StmtOutput[F] = execute1(body)
+            Monad[F].whileM(conditionStmt)(bodyStmt)
+
+          case If(cond, thenBranch, elseBranch) =>
+            for
+              state <- env.state
+              result <- interpreter.evaluate(state, cond)
+              _ <-
+                if result.isTruthy then execute(thenBranch)
+                else elseBranch.fold(Monad[F].unit.widen)(eb => execute(eb))
+            yield ()
+
+      def execute1(stmts: List[Stmt]): F[Stream[F, Output]] = ???
