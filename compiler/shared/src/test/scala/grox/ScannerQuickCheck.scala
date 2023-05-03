aacef492b63de4459f662f2d69e0a8863c087a73
Thanh Le <lenguyenthanh@hotmail.com>
2023-05-03 18:21:33 +0200

    Manual rewrite for scala-3.3.0

--- compiler/shared/src/test/scala/grox/ScannerQuickCheck.scala
+++ compiler/shared/src/test/scala/grox/ScannerQuickCheck.scala
@@ -12,18 +12,16 @@ class ScannerQuickCheck extends ScalaCheckSuite:
   // and put them together as input
   // our sanner should parse it succesfully
   property("parse succesfully"):
-    Prop.forAll(TokenGen.tokensGen) { ts =>
+    Prop.forAll(TokenGen.tokensGen): ts =>
       val lox = ts.foldMap(identity)
       Scanner.parse(lox).isRight == true
-    }
 
   // generate random list of tokens
   // and put them together as input
   // our sanner should parse it succesfully
   // and the result should have the same size as the input
   property("parsed tokens should have the same size as input"):
-    Prop.forAll(TokenGen.tokensGen) { ts =>
+    Prop.forAll(TokenGen.tokensGen): ts =>
       val lox = ts.foldMap(identity)
       val resultSize = Scanner.parse(lox).map(_.size)
       resultSize == Right(ts.size)
-    }
