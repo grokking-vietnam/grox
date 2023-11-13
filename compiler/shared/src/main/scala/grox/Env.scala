e6f86ff0f706fbfd1f966dfbb8d68df0078c6ef5
Thanh Le <lenguyenthanh@hotmail.com>
2023-11-13 19:22:39 +0100

    Refactor Env.assign using flatModify

--- compiler/shared/src/main/scala/grox/Env.scala
+++ compiler/shared/src/main/scala/grox/Env.scala
@@ -4,6 +4,8 @@ import scala.util.control.NoStackTrace
 import cats.MonadThrow
 import cats.effect.kernel.Ref
 import cats.syntax.all.*
+import cats.Monad
+import cats.effect.kernel.MonadCancel
 
 case class EnvError(msg: String) extends NoStackTrace
 
@@ -17,21 +19,23 @@ trait Env[F[_]]:
 
 object Env:
 
-  def instance[F[_]: MonadThrow: Ref.Make](s: State): F[Env[F]] = Ref[F].of(s).map { ref =>
-    new:
-      // we allowed redeclaration of variables
-      def define(name: String, value: LiteralType): F[Unit] = ref.update(s => s.define(name, value))
-      def assign(name: String, value: LiteralType): F[Unit] =
-        for
-          (s, set) <- ref.access
-          ss <- s.assign(name, value).liftTo[F]
-          success <- set(ss)
-          _ <-
-            if success then MonadThrow[F].unit
-            else MonadThrow[F].raiseError(EnvError(s"Cannot update program state."))
-        yield ()
-      def get(name: String): F[LiteralType] = ref.get.map(_.get(name).liftTo[F]).flatten
-      def state: F[State] = ref.get
-      def startBlock(): F[Unit] = ref.update(s => State(Map.empty, Some(s)))
-      def endBlock(): F[Unit] = ref.update(_.enclosing.getOrElse(State()))
-  }
+  def instance[F[_]: Ref.Make](
+    using MonadCancel[F, Throwable]
+  )(
+    s: State
+  ): F[Env[F]] = Ref[F]
+    .of(s)
+    .map: ref =>
+      new:
+        // we allowed redeclaration of variables
+        def define(name: String, value: LiteralType): F[Unit] =
+          ref.update(s => s.define(name, value))
+
+        def assign(name: String, value: LiteralType): F[Unit] = ref.flatModify: s =>
+          s.assign(name, value) match
+            case Right(ss) => (ss, Monad[F].unit)
+            case Left(ex)  => (s, MonadThrow[F].raiseError(ex))
+        def get(name: String): F[LiteralType] = ref.get.map(_.get(name).liftTo[F]).flatten
+        def state: F[State] = ref.get
+        def startBlock(): F[Unit] = ref.update(s => State(Map.empty, Some(s)))
+        def endBlock(): F[Unit] = ref.update(_.enclosing.getOrElse(State()))
