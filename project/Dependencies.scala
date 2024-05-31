eb5b6a07b7ee252521c61d8725c2b2b272b38313
Thanh Le <lenguyenthanh@hotmail.com>
2024-05-31 13:28:14 +0700

    Update scalacheck-effect

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,10 +26,11 @@ object Dependencies {
   val munitCatsEffect = Def.setting("org.typelevel" %%% "munit-cats-effect" % "2.0.0" % Test)
   val scalaCheck = Def.setting("org.scalacheck" %%% "scalacheck" % "1.16.0" % Test)
   val munitScalaCheck = Def.setting("org.scalameta" %%% "munit-scalacheck" % "1.0.0" % Test)
-  val scalaCheckEffect = Def.setting("org.typelevel" %%% "scalacheck-effect" % "1.0.4" % Test)
+  val scalaCheckEffect = Def.setting("org.typelevel" %%% "scalacheck-effect" % 2.0.0-M2" % Test)
 
   val munitScalaCheckEffect = Def.setting(
-    "org.typelevel" %%% "scalacheck-effect-munit" % "1.0.4" % Test
+    "org.typelevel" %%% "scalacheck-effect-munit" % "2.0.0-M2" % Test
+
   )
 
 }
